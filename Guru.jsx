1b404869eeef72df6df7c4eff16ed3746a0e370b
rkiaaaf <87561930+rkiaaaf@users.noreply.github.com>
2026-05-21 20:51:31 +0700

    Update Guru.jsx

--- Guru.jsx
+++ Guru.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Card, Badge, Avatar, Input, Select, Toast, StarRating, IuranBanner,
   formatDate, formatRupiah, generateId, generateCode, getInitials, avatarColor,
